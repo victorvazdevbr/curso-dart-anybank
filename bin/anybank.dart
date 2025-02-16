78b9b1d2c7799303a7682d4b5832834ce4d04420
Victor Vaz <contato@victorvaz.dev.br>
2025-02-15 23:06:43 -0300

    Classe Conta transformada em classe abstrata

--- bin/anybank.dart
+++ bin/anybank.dart
@@ -1,25 +1,12 @@
-import 'package:anybank/conta.dart';
 import 'package:anybank/conta_corrente.dart';
 import 'package:anybank/conta_poupanca.dart';
 import 'package:anybank/conta_salario.dart';
 
 void main() {
-  Conta contaMatheus = Conta('Matheus', 1000);
-  Conta contaRoberta = Conta('Roberta', 2000);
   ContaCorrente contaChris = ContaCorrente('Chris', 4000);
   ContaPoupanca contaDenize = ContaPoupanca('Denize', 4000);
   ContaSalario contaCatarina =
       ContaSalario('Catarina', 5000, 'Empresa X', '101010101000110');
-
-  List<Conta> contas = <Conta>[contaMatheus, contaRoberta];
-
-  for (Conta conta in contas) {
-    conta.imprimeSaldo();
-  }
-
-  contaRoberta.setSaldo(5000);
-  contaMatheus.receber(500);
-  contaMatheus.enviar(200);
 
   contaChris.imprimeSaldo();
   contaChris.enviar(4300);
